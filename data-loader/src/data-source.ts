be02ddd464fb9aa22e48d3905093ae0b3be35964
rafaelrcarvalho <rrodriguesdecarvalho@gmail.com>
2025-04-01 21:10:20 -0300

    Fix env load

--- data-loader/src/data-source.ts
+++ data-loader/src/data-source.ts
@@ -2,23 +2,15 @@ import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { UserConsumption } from "./entity/userConsumption";
 
+require("dotenv").config();
+
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
   synchronize: true,
   logging: true,
   entities: [UserConsumption],
 });
-
-AppDataSource.initialize()
-  .then(() => {
-    // here you can start to work with your database
-  })
-  .catch((error) => console.log(error));
-
-export const UserConsumptionRepository =
-  AppDataSource.getMongoRepository(UserConsumption);
